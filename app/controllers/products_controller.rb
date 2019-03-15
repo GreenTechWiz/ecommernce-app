af925e56dd9c273615f34c95f16b1508e2383949
GreenTechWiz <aggreen0405@gmail.com>
2019-03-14 20:54:19 -0500

    Add New and Create actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,18 @@ class ProductsController < ApplicationController
     #We set a @ varible, calling out Product model and all of them IE why we use .all
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    product = Product.new(name: params[:name], description: params[:description], price: params[:price])
+    product.save
+    redirect_to "/products/#{product.id}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   #Calls products databases, using the index method we made in the index.html.erb file
-  get "/" => 'products#index' 
-  get "/products" => 'products#index'
+  get '/' => 'products#index' 
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post 'products' => 'products#create'
+  get '/products/:id' => 'products#show'
 end
